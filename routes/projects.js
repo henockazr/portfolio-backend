67f4570e07836fe100e65865992017946f8a3d59
henockazr <azrielhenock@yahoo.com>
2025-12-27 12:12:48 +0700

    Fixes: Add Middleware on Project

--- routes/projects.js
+++ routes/projects.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getProject, getProjectId, createProject, updateProject, deleteProject } from "../controllers/projectController.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/", getProject);
 router.get("/:id", getProjectId);
-router.post("/", createProject);
-router.put("/:id", updateProject);
-router.delete("/:id", deleteProject);
+router.post("/", authenticateToken, createProject);
+router.put("/:id", authenticateToken, updateProject);
+router.delete("/:id", authenticateToken, deleteProject);
 
 export default router;
